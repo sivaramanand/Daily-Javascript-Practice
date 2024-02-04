95552f0ebfe8b61a3a2413ccfbbaeaee522abec0
Sivaram <52231444+sivaramanand@users.noreply.github.com>
2024-02-04 17:23:51 +0530

    Add files via upload

---
+++ revising javascript/1.variables and datatypes/first program.js
@@ -0,0 +1,58 @@
+// fullName='Tony Stark';
+// age=24;
+// price=99.99;
+// radius=14;
+// a=null;
+// y=undefined;
+// isFollow=false;
+// fullName=25;
+
+
+// console.log(fullName);
+// console.log(age);
+// console.log(price); 
+// console.log(radius);    
+// console.log(a);
+// console.log(y);
+// console.log(isFollow);
+// console.log(fullName);
+
+// fullname="sivaram";
+// fullname="anandkumar";
+
+// console.log(fullname);
+
+// var PI=3.15
+// var PI=7.98
+// console.log(PI);
+
+// const students={
+//     name:"sivaram",
+//     age:24,
+//     gender:"male",
+//     isLoser:false
+// }
+
+// console.log(students.age)
+// console.log(students['age'])
+
+
+// let number=prompt("enter your score")
+
+// if(number>=80 && number<=100){
+//     console.log("A")
+// }else if(number>=70 && number<=79)
+// {
+//     console.log("B")
+// }
+// else if(number>=60 && number<=69){
+//     console.log("c")
+// }
+// else if(number>=50 && number<=59){
+//     console.log("D")
+// }else if(number>=0 && number<=49){
+//     console.log("F")
+// }
+// else {
+//     console.log("Invalid Input")
+// }
---
+++ revising javascript/3.loops and strings/for loop.js
@@ -0,0 +1,15 @@
+// let sum=0;
+// for(var i=0;i<10;i++){
+// sum=sum+i
+// console.log(sum)
+// var consatant=23
+// }
+// console.log(consatant)
+
+// whileloop 
+
+let  i=0
+do{
+    i=i+1
+    console.log(1)
+}while( i <0)

fe49241533b2c682531843115f1c64cbfca904ce
Sivaram <52231444+sivaramanand@users.noreply.github.com>
2024-02-10 19:10:29 +0530

    Add files via upload

---
+++ revising javascript/4.arrays/script.js
@@ -0,0 +1,12 @@
+// const cities=['cbe','mdu','chennai','bgr','pondy']
+
+// cities.map((items)=>(console.log(items.match("bgr"))))
+
+
+const marks=[85,97,44,37,76,60]
+let sum=0
+for ( i=0;i<marks.length;i++)
+{
+sum=sum+marks[i]
+}
+console.log(sum/marks.length)

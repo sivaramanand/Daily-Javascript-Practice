edbfcc7f79f91766cea44aa44f10a40d7dd69eb4
Sivaram <52231444+sivaramanand@users.noreply.github.com>
2024-03-18 22:24:28 +0530

    Add files via upload

--- 3.Weight Converter/script.js
+++ 3.Weight Converter/script.js
@@ -4,5 +4,5 @@ const output=document.querySelector(".outputlabel")
 input.addEventListener("input",()=>{ 
     let vars = input.value;
     let val = vars*2.2;
-    output.innertext=val.toFixed(2)
+    output.innerHTML=val.toFixed(2)
    })
